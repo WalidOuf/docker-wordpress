b41ad4cdc0d6ef172115798ff88fa00684ead31e
Walid Oufrout <oufrout.walid@gmail.com>
2023-08-09 11:48:48 +0100

    Initial commit

---
+++ wp-content/themes/newtheme/template-parts/footer-content.php
@@ -0,0 +1,5 @@
+<footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-e378">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <p class="u-small-text u-text u-text-variant u-text-1">Exemple de texte. Cliquez pour sélectionner l'élément de texte.</p>
+  </div>
+</footer>
